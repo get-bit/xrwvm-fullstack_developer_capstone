e87e74c5c2cfd76672a055297afa95e6d59c829f
“get-bit” <maya.chump_0n@icloud.com>
2025-03-26 16:57:35 -0400

    CarModel and CarMake models added

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,6 +8,7 @@ app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
     path(route='register', view=views.registration, name='register'),
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
@@ -16,5 +17,6 @@ urlpatterns = [
     # path for dealer reviews view
 
     # path for add a review view
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

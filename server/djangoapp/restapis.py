97e5d6d0f894e21c039663ee623f3caf33be48f5
“get-bit” <maya.chump_0n@icloud.com>
2025-04-30 11:42:35 -0400

    new backend cars inventory

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -10,6 +10,9 @@ backend_url = os.getenv(
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
+searchcars_url = os.getenv(
+    'searchcars_url',
+    default="http://localhost:3050/")
 
 
 def get_request(endpoint, **kwargs):
@@ -50,3 +53,23 @@ def post_review(data_dict):
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred. Code 3")
+
+
+def searchcars_request(endpoint, **kwargs):
+    params = ""
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params+key + "=" + value + "&"
+
+    request_url = searchcars_url+endpoint+"?"+params
+
+    print("GET from {} ".format(request_url))
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("Network exception occurred. Code 4")
+    finally:
+        print("GET request call complete!")
